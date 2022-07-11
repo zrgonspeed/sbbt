1d9c3c6ce2d6a336fef60945979a0e84ca067e2d
zhangriguang <zhangriguang@chuhui.com>
2022-07-11 17:38:19 +0800

    1.update

--- app/src/main/java/com/run/treadmill/bluetooth/BleDebug.java
+++ app/src/main/java/com/run/treadmill/bluetooth/BleDebug.java
@@ -1,6 +1,6 @@
 package com.run.treadmill.bluetooth;
 
 public class BleDebug {
-    public static final boolean debug = false;
+    public static final boolean blueDebug = false;
     public static final boolean disableSerial = false;
 }
