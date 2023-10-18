f49d1e3a916b8e5f180f705cd00c40b4c0d77dfa
zhangriguang <zhangriguang@chuhui.com>
2023-10-18 09:19:13 +0800

    1.取消otamcu上电安装

--- app/src/main/java/com/run/treadmill/base/MyApplication.java
+++ app/src/main/java/com/run/treadmill/base/MyApplication.java
@@ -155,7 +155,7 @@ public class MyApplication extends LitePalApplication {
         MusicLight.startThread();
         MusicReceiverManager.register();
 
-        OtaMcuUtils.installOtaMcu(this);
+        // OtaMcuUtils.installOtaMcu(this);
     };
 
 
