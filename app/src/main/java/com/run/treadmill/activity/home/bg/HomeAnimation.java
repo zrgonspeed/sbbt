b95846430b3f4ea1f7d6204d698ad9ed11b25698
zhangriguang <zhangriguang@chuhui.com>
2024-03-12 14:12:20 +0800

    1.ResourceUtils

--- app/src/main/java/com/run/treadmill/activity/home/bg/HomeAnimation.java
+++ app/src/main/java/com/run/treadmill/activity/home/bg/HomeAnimation.java
@@ -7,8 +7,9 @@ import android.os.Message;
 import android.widget.ImageView;
 
 import com.run.treadmill.R;
-import com.run.treadmill.reboot.MyApplication;
+import com.run.treadmill.util.ResourceUtils;
 
+// 图片轮播图，淡入淡出
 public class HomeAnimation {
     private ImageView iv_home_bg;
 
@@ -20,43 +21,36 @@ public class HomeAnimation {
         this.iv_home_bg = iv_home_bg;
     }
 
-    // 图片轮播图，淡入淡出
     public void initAndStart() {
-        initDrawableView();
         thread = new Thread(new MyRunnable());
         thread.start();
     }
 
-    private int change = 0;//记录下标
-    private int[] ids = new int[]{
-            R.drawable.bk_background_idle_mode_1,
-            R.drawable.bk_background_idle_mode_2,
-            R.drawable.bk_background_idle_mode_3,
-            R.drawable.bk_background_idle_mode_4,
-            R.drawable.bk_background_idle_mode_5
-
+    private Drawable[] drawables = new Drawable[]{
+            ResourceUtils.getDraw(R.drawable.bk_background_idle_mode_1),
+            ResourceUtils.getDraw(R.drawable.bk_background_idle_mode_2),
+            ResourceUtils.getDraw(R.drawable.bk_background_idle_mode_3),
+            ResourceUtils.getDraw(R.drawable.bk_background_idle_mode_4),
+            ResourceUtils.getDraw(R.drawable.bk_background_idle_mode_5)
     };
-    private Drawable[] drawables;//图片集合
-    private Thread thread;//线程
-    private boolean threadFlag = true;//线程结束标志符
+    private Drawable[] arr = new Drawable[2];
+    private int change = 0;     //记录下标
 
-    private void initDrawableView() {
-        //填充图片
-        drawables = new Drawable[ids.length];
-        for (int i = 0; i < ids.length; i++) {
-            drawables[i] = MyApplication.getContext().getResources().getDrawable(ids[i]);
-        }
-    }
+    private Thread thread;
+    private boolean threadRun = true;   //线程结束标志符
+    private int duration = 2000;
+    private int sleep = 5000;
 
     private Handler mHandler = new Handler(new Handler.Callback() {
         @Override
         public boolean handleMessage(Message msg) {
-            int duration = msg.arg1;
-            TransitionDrawable transitionDrawable = new TransitionDrawable(new Drawable[]{drawables[change % ids.length],
-                    drawables[(change + 1) % ids.length]});
-            change++;//改变标识位置
+            arr[0] = drawables[change % drawables.length];
+            arr[1] = drawables[(change + 1) % drawables.length];
+            TransitionDrawable transitionDrawable = new TransitionDrawable(arr);
             iv_home_bg.setBackground(transitionDrawable);
             transitionDrawable.startTransition(duration);
+
+            change++;
             return false;
         }
     });
@@ -64,24 +58,21 @@ public class HomeAnimation {
     private class MyRunnable implements Runnable {
         @Override
         public void run() {
-            //这个while(true)是做死循环
-            while (threadFlag) {
-                int duration = 2000;//改变的间隔
-                Message message = mHandler.obtainMessage();
-                message.arg1 = duration;
-                mHandler.sendMessage(message);
-                try {
-                    Thread.sleep(5000);
-                    //隔duration秒发送一次
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+            try {
+                while (threadRun) {
+                    Message message = mHandler.obtainMessage();
+                    message.arg1 = duration;
+                    mHandler.sendMessage(message);
+                    Thread.sleep(sleep);
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
 
     public void destroy() {
-        threadFlag = false;
+        threadRun = false;
         if (thread != null) {
             thread.interrupt();
             thread = null;
---
+++ app/src/main/java/com/run/treadmill/util/ResourceUtils.java
@@ -0,0 +1,19 @@
+package com.run.treadmill.util;
+
+import android.graphics.drawable.Drawable;
+
+import com.run.treadmill.reboot.MyApplication;
+
+public class ResourceUtils {
+    public static Drawable getDraw(int id) {
+        return MyApplication.getContext().getDrawable(id);
+    }
+
+    public static String getString(int id) {
+        return MyApplication.getContext().getString(id);
+    }
+
+    public static int getColor(int id) {
+        return MyApplication.getContext().getColor(id);
+    }
+}
