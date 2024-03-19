fe813d220d72fc478d89e22eba56ac70c80f2b7d
zhangriguang <zhangriguang@chuhui.com>
2024-03-19 19:55:01 +0800

    graph click

--- app/src/main/java/com/run/treadmill/activity/runMode/help/RunGraph.java
+++ app/src/main/java/com/run/treadmill/activity/runMode/help/RunGraph.java
@@ -6,7 +6,7 @@ import android.widget.RadioButton;
 import com.run.treadmill.R;
 import com.run.treadmill.activity.runMode.BaseRunActivity;
 import com.run.treadmill.sp.SpManager;
-import com.run.treadmill.util.Logger;
+import com.run.treadmill.util.RunGraphUtils;
 import com.run.treadmill.widget.chart.SportGraph;
 
 /**
@@ -22,22 +22,31 @@ public class RunGraph {
 
         rb_graph_left = bar_graph.findViewById(R.id.rb_graph_left);
         rb_graph_right = bar_graph.findViewById(R.id.rb_graph_right);
+        ClickRadio clickRadio = new ClickRadio();
+        rb_graph_left.setOnClickListener(clickRadio);
+        rb_graph_right.setOnClickListener(clickRadio);
 
-        /*扬升和速度点击切换事件*/
-        bar_graph.setOnGraphTypeChangeListener((group, checkedId) -> {
-            if (checkedId == R.id.rb_graph_left) {
-                Logger.i("checkedId == 点扬升，要切为速度");
-                checkedSpeed();
-            }
-            if (checkedId == R.id.rb_graph_right) {
-                Logger.i("checkedId == 点速度, 要切为扬升");
-                checkedIncline();
-            }
-        });
-
-        // bar_graph.setCheckBarGraph(0);
+        // 默认先显示速度
         bar_graph.setCurrSelectType(1);
         checkedSpeed();
+    }
+
+    private class ClickRadio implements View.OnClickListener {
+        @Override
+        public void onClick(View v) {
+            if (!RunGraphUtils.canResponse()) {
+                return;
+            }
+
+            if (v.getId() == R.id.rb_graph_left) {
+                // Logger.i("checkedId == 点扬升，要切为速度");
+                checkedSpeed();
+            }
+            if (v.getId() == R.id.rb_graph_right) {
+                // Logger.i("checkedId == 点速度, 要切为扬升");
+                checkedIncline();
+            }
+        }
     }
 
     private int getMaxIncline() {
@@ -111,18 +120,6 @@ public class RunGraph {
     public boolean curIsSpeed() {
         return bar_graph.getCurrSelectType() == 1;
     }
-/*
-    private GraphRefresh graphRefresh;
-
-    public void setGraphRefresh(GraphRefresh graphRefresh) {
-        this.graphRefresh = graphRefresh;
-    }
-
-    interface GraphRefresh {
-        void inclineRefresh();
-
-        void speedRefresh();
-    }*/
 
     private BaseRunActivity ac;
 
